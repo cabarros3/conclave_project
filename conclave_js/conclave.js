4ab3b50fe2202c03078262fdb29ab7c84b4a0ce1
Camilla Barros <camillaerbarros@gmail.com>
2025-08-27 06:05:32 -0300

    refactor: melhora a estrutura da função vote

--- conclave_js/conclave.js
+++ conclave_js/conclave.js
@@ -17,20 +17,22 @@ console.log(
 let cardeais = []; // salva os nomes e números dos cardeais em um vetor (uma lista de objetos)
 
 // for para pegar os cardeais e guardar no array
-for (let i = 0; i < 5; i++) {
-  let nome = entrada(`Type the name of the cardeal ${i + 1}: `);
-  let cardeal = {
-    id: i + 1,
-    nome: nome,
-    votos: 0, // cardeais começam com 0 votos (todos)
-  };
-  cardeais.push(cardeal);
-  console.log(
-    `The cardeal ${cardeal.nome} (ID: ${cardeal.id}) was insert into the system`
-  );
+function nominateCardinals() {
+  for (let i = 0; i < 5; i++) {
+    let nome = entrada(`Type the name of the cardeal ${i + 1}: `);
+    let cardeal = {
+      id: i + 1,
+      nome: nome,
+      votos: 0, // cardeais começam com 0 votos (todos)
+    };
+    cardeais.push(cardeal);
+    console.log(
+      `The cardeal ${cardeal.nome} (ID: ${cardeal.id}) was insert into the system`
+    );
+  }
 }
 
-console.log(cardeais);
+// console.log(cardeais);
 
 // inicio da votação
 console.log("====================================");
@@ -39,34 +41,35 @@ console.log("====================================");
 
 console.log("This is an anonymous election");
 
-for (let i = 0; i < cardeais.length; i++) {
-  console.log("List of cardinales up to election: ");
-  cardeais.forEach((c) => console.log(`${c.id} - ${c.nome}`));
+function vote() {
+  for (let i = 0; i < cardeais.length; i++) {
+    console.log("List of cardinales up to election: ");
+    cardeais.forEach((c) => console.log(`${c.id} - ${c.nome}`));
 
-  let voto = Number(entrada("Please, insert your vote: "));
+    let voto = Number(entrada("Please, insert your vote: "));
 
-  switch (voto) {
-    case 1:
-      cardeais[0].votos++;
-      break;
-    case 2:
-      cardeais[1].votos++;
-      break;
-    case 3:
-      cardeais[2].votos++;
-      break;
-    case 4:
-      cardeais[3].votos++;
-      break;
-    case 5:
-      cardeais[4].votos++;
-      break;
-    default:
-      console.log("Ivalid insert");
+    // Verifica se o voto é válido
+    if (voto >= 1 && voto <= cardeais.length) {
+      cardeais[voto - 1].votos++; // usa o índice correspondente, id começa em 1, mas indice começa em 0. então subtrai para pegar o índice.
+    } else {
+      console.log("Invalid vote");
+      i--; // repete a votação deste cardeal
+    }
   }
 }
 
-console.log("\nResultado da votação:");
-cardeais.forEach((c) => console.log(` Cardinale ${c.nome}: ${c.votos} votos`));
+// função para contar os votos
+function countVotes() {
+  console.log("\nResultado da votação:");
+  cardeais.forEach((c) =>
+    console.log(` Cardinale ${c.nome}: ${c.votos} votos`)
+  );
+}
 
-function newPope() {}
+// função para mostrar o novo papa
+function showNewPope() {}
+
+// chamando as funções
+nominateCardinals();
+vote();
+countVotes();
